f10bb48ca983fbe24e894d877d2224b91263c69c
wAiwAi <822458398@qq.com>
2022-06-07 22:56:13 +0800

    6.7

--- HJ43 迷宫问题.js
+++ HJ43 迷宫问题.js
@@ -1,41 +1,38 @@
-const [len1, len2] = readline().split(" ");
+const [len1, len2] = readline().split(" ").map(Number);
 const arr = [];
-for (let i = 0; i < len1; i++) {
-  const itemArr = readline().split(" ");
-  arr.push(itemArr);
+while ((input = readline())) {
+  arr.push(input.split(" ").map(Number));
 }
-console.log(arr);
-const pathArr = [[0, 0]];
-let flag = true;
-let i = 0;
-let j = 0;
-// 递归
-pathHandle();
-function pathHandle(path = [0, 0]) {
-  if (path[parseInt(len1)][parseInt(len2)] == 0) {
-    pathArr = path;
-  } else {
-    move(1, path);
-    move(2, path);
-    move(3, path);
-    move(4, path);
-  }
-}
-function move(type, path) {
-  if (type == 1 && arr[i][j + 1] == 0) {
-    path.push([i, j + 1]);
-    pathHandle(path);
-  } else if (type == 2 && arr[i][j - 1] == 0) {
-    path.push([i, j + 1]);
-    pathHandle(path);
-  } else if (type == 3 && arr[i + 1][j] == 0) {
-    path.push([i + 1, j]);
-    pathHandle(path);
-  } else if (type == 4 && arr[i - 1][j] == 0) {
-    path.push([i - 1, j]);
-    pathHandle(path);
-  }
-}
-pathArr.forEach((item) => {
-  console.log(`(${item[0]},${item[1]})`);
+const doneList = Array.from({ length: len1 }, () => {
+  return new Array(len2).fill(0);
 });
+const res = [];
+let arr1 = [1, 0, -1, 0];
+let arr2 = [0, 1, 0, -1];
+movePath([[0, 0]], 0, 0);
+// 递归
+function movePath(pathArr, m, n) {
+  if (m == len1 - 1 && n == len2 - 1) {
+    pathArr.map((item) => {
+      console.log(`(${item[0]},${item[1]})`);
+    });
+    return;
+  }
+  let i = 0;
+  const curPath = pathArr.slice();
+  while (i < 4) {
+    let curX = n + arr2[i];
+    let curY = m + arr1[i];
+    if (curX >= 0 && curY >= 0 && curX < len2 && curY < len1) {
+      if (arr[curY][curX] == 0 && doneList[curY][curX] == 0) {
+        doneList[curY][curX] = 1;
+        curPath.push([curY, curX]);
+        movePath(curPath, curY, curX);
+        curPath.pop();
+        doneList[curY][curX] = 0;
+      }
+    }
+    i++;
+  }
+  return;
+}
