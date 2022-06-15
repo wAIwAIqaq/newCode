6ff646063cbfad3212611e19270a0cbf76ace5e6
wAiwAi <822458398@qq.com>
2022-06-15 21:51:13 +0800

    6.15

--- HJ89 24点运算.js
+++ HJ89 24点运算.js
@@ -1,80 +1,60 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.on("line", function (line) {
-  const arr = line.split(" ");
-  if (arr.includes("joker") || arr.includes("JOKER")) {
-    console.log("ERROR");
-    return;
-  }
+const arr = readline().split(" ");
+if (arr.includes("joker") || arr.includes("JOKER")) {
+  console.log("ERROR");
+} else {
+  const map = new Map([
+    ["A", 1],
+    ["2", 2],
+    ["3", 3],
+    ["4", 4],
+    ["5", 5],
+    ["6", 6],
+    ["7", 7],
+    ["8", 8],
+    ["9", 9],
+    ["10", 10],
+    ["J", 11],
+    ["Q", 12],
+    ["K", 13],
+  ]);
+  let res = "NONE";
   let i = 0;
-  const res = [];
-  toCacul([arr[1], arr[2], arr[3]], toNum(arr[0]), arr[0]);
-  toCacul([arr[0], arr[2], arr[3]], toNum(arr[1]), arr[1]);
-  toCacul([arr[0], arr[1], arr[3]], toNum(arr[2]), arr[2]);
-  toCacul([arr[0], arr[1], arr[2]], toNum(arr[3]), arr[3]);
-  if (res.length == 0) {
-    console.log("NONE");
-  } else {
-    console.log(res[0]);
+  while (i < 4) {
+    const nextRest = [...arr.slice(0, i), ...arr.slice(i + 1, arr.length)];
+    getVal(nextRest, map.get(arr[i]), arr[i]);
+    i++;
   }
-  function toCacul(arr, val = 0, str = "") {
-    if (val == 24 && arr.length == 0) {
-      res.push(str);
-      return;
-    } else if (arr.length == 0) {
+  console.log(res);
+  function getVal(rest, val = 0, str = "") {
+    if (val == 24 && rest.length == 0) {
+      res = str;
       return;
     }
-    toAdd(arr, val, str);
-    toSub(arr, val, str);
-    toMul(arr, val, str);
-    toDiv(arr, val, str);
-  }
-  function toNum(arr) {
-    let cur;
-    if (arr[0] == "A") {
-      cur = 1;
-    } else if (arr[0] == "J") {
-      cur = 11;
-    } else if (arr[0] == "Q") {
-      cur = 12;
-    } else if (arr[0] == "K") {
-      cur = 13;
+    if (rest.length) {
+      let i = 0;
+      while (i < rest.length) {
+        const nextRest = [
+          ...rest.slice(0, i),
+          ...rest.slice(i + 1, rest.length),
+        ];
+        let curVal = val;
+        curVal += map.get(rest[i]);
+        getVal(nextRest, curVal, `${str}+${rest[i]}`);
+        curVal = val;
+        curVal -= map.get(rest[i]);
+        getVal(nextRest, curVal, `${str}-${rest[i]}`);
+        curVal = val;
+        curVal *= map.get(rest[i]);
+        getVal(nextRest, curVal, `${str}*${rest[i]}`);
+        curVal = val;
+        curVal /= map.get(rest[i]);
+        curVal = Math.floor(curVal);
+        getVal(nextRest, curVal, `${str}/${rest[i]}`);
+        curVal = val;
+        i++;
+      }
     } else {
-      cur = parseInt(arr[0]);
+      return;
     }
-    return cur;
   }
-  function toAdd(arr, val, str = "") {
-    let cur = toNum(arr);
-    val += cur;
-    str += `+${str[0]}`;
-    const subArr = arr.slice();
-    toCacul(subArr.splice(i, 1), val, str);
-  }
-
-  function toSub(arr, val, str = "") {
-    let cur = toNum(arr);
-    val -= cur;
-    str += `-${str[0]}`;
-    const subArr = arr.slice();
-    toCacul(subArr.splice(i, 1), val, str);
-  }
-  function toMul(arr, val, str = "") {
-    let cur = toNum(arr);
-    val *= cur;
-    str += `*${str[0]}`;
-    const subArr = arr.slice();
-    toCacul(subArr.splice(i, 1), val, str);
-  }
-  function toDiv(arr, val, str = "") {
-    let cur = toNum(arr);
-    val /= cur;
-    str += `/${str[0]}`;
-    const subArr = arr.slice();
-    toCacul(subArr.splice(i, 1), val, str);
-  }
-});
+}
--- index.js
+++ index.js
@@ -1,49 +1,29 @@
-/**
- * max length of the subarray sum = k
- * @param arr int整型一维数组 the array
- * @param k int整型 target
- * @return int整型
- */
-function maxlenEqualK(arr, k) {
+function permute(nums) {
   // write code here
-  let itemSum = 0;
-  let res = 0;
-  const sumArr = arr.map((item) => {
-    return (itemSum += item);
-  });
-  // 转化为 求t两数之差 = k
-  const map = new Map();
-  console.log(sumArr);
-  sumArr.forEach((item, index) => {
-    map.set(item, index);
-  });
-  sumArr.forEach((item, index) => {
-    if (item === k) {
-      res = Math.max(index + 1, res);
+  const res = [];
+  getAllSort(nums, []);
+  function getAllSort(nums, curArr) {
+    if (nums.length != 0) {
+      let i = 0;
+      while (i < nums.length) {
+        const nextNums = nums.slice();
+        curArr.push(nums[i]);
+        nextNums.splice(i, 1);
+        getAllSort(nextNums, curArr);
+        debugger;
+        curArr.pop();
+        i++;
+      }
     } else {
-      const target1 = k - item;
-      const target2 = item - k;
-      const targetIndex1 = map.get(target1);
-      const targetIndex2 = map.get(target2);
-      // 求绝对值
-      if (targetIndex1) {
-        const sublen =
-          index > targetIndex1
-            ? parseInt(index) - parseInt(targetIndex1)
-            : parseInt(targetIndex1) - parseInt(index);
-        res = Math.max(sublen, res);
-      }
-      if (targetIndex2) {
-        const sublen =
-          index > targetIndex2
-            ? parseInt(index) - parseInt(targetIndex2)
-            : parseInt(targetIndex2) - parseInt(index);
-        res = Math.max(sublen, res);
-      }
+      res.push([...curArr]);
+      return;
     }
-  });
+  }
   return res;
 }
-module.exports = {
-  maxlenEqualK: maxlenEqualK,
-};
+permute([1, 2, 3, 4 , 5, 6]);
+          cur            nums
+         [1,2,3]        [4,5,6];
+i = 0    [1,2,3,4]      [5,6]      delete 4
+i = 1    [1,2,3,5]      [4,6]      delete 5
+i = 2    [1,2,3,6]      [4,5]      delete 6
