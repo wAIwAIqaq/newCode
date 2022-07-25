1c28409dd3c1a3982da94545643bea92ab39c89f
wAiwAi <822458398@qq.com>
2022-07-25 21:39:22 +0800

    7.25

---
+++ LeetCode/88. 合并两个有序数组.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = 0;
+  while (i < n) {
+    nums1.unshift(nums2[i]);
+    nums1.pop();
+    i++;
+  }
+  nums1.sort((a, b) => a - b);
+};
